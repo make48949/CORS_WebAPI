b630a4e065e22982d2017b5630f5ae79350844ba
Mapendo Kelly <make48949@edu.ucl.dk>
2023-10-23 10:33:32 +0200

    Add project files.

---
+++ CORS_WebAPI/Program.cs
@@ -0,0 +1,52 @@
+namespace CORS_WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //Fixing CORS Problem
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyAllowedOrigins",
+                    policy =>
+                    {
+                        policy.WithOrigins("https://localhost:8081") // note the port is included 
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            //var app = builder.Build();
+            var app = builder.Build(); // this will already be in the file
+            app.UseCors("MyAllowedOrigins");
+
+            
+           
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ CORS_WebAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CORS_WebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
